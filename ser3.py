18bf8e00030f7b566f662aab60796ffc923e8bbd
Nesya <angerasimov04@outlook.com>
2021-02-28 14:35:44 +0300

    task 3

--- ser3.py
+++ ser3.py
@@ -16,5 +16,10 @@ def training(prof):
     return render_template('n.html', prof=prof)
 
 
+@app.route('/list_prof/<list>')
+def prof_list(list):
+    return render_template('prof_list.html', list=list)
+
+
 if __name__ == '__main__':
     app.run(port=8000, host='127.0.0.1')
