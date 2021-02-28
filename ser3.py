e2d68ca40ca4a69356bbf3418bdae0031b45b922
Nesya <angerasimov04@outlook.com>
2021-02-28 15:53:12 +0300

    task 4

--- ser3.py
+++ ser3.py
@@ -21,5 +21,19 @@ def prof_list(list):
     return render_template('prof_list.html', list=list)
 
 
+@app.route('/auto_answer')
+@app.route('/answer')
+def answer():
+    param = {}
+    param['surname'] = "Watny"
+    param['name'] = "Mark"
+    param['education'] = 'выше среднего'
+    param['profession'] = 'штурман марсохода'
+    param['sex'] = 'male'
+    param['motivation'] = 'Всегда мечтал застрять на Марсе!'
+    param['ready'] = True
+    return render_template('auto_answer.html', param=param)
+
+
 if __name__ == '__main__':
     app.run(port=8000, host='127.0.0.1')
